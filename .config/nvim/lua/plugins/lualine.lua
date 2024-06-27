a798784e073cf4dff6ac338043bcbb58f2a86480
Ushira Dineth <ushiradineth@gmail.com>
2024-06-28 01:26:24 +0400

    feat(lualine): updated lualine theme

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -68,17 +68,13 @@ return {
       },
     }
 
+    a = 2
+
     -- Inserts a component in lualine_c at left section
     local function ins_left(component) table.insert(config.sections.lualine_c, component) end
 
     -- Inserts a component in lualine_x at right section
     local function ins_right(component) table.insert(config.sections.lualine_x, component) end
-
-    ins_left {
-      function() return "▊" end,
-      color = { fg = colors.blue }, -- Sets highlighting of component
-      padding = { left = 0, right = 1 }, -- We don't need space before this
-    }
 
     ins_left {
       -- mode component
@@ -109,24 +105,12 @@ return {
         }
         return { fg = mode_color[vim.fn.mode()] }
       end,
-      padding = { right = 1 },
+      padding = { left = 1, right = 1 },
     }
-
-    ins_left {
-      -- filesize component
-      "filesize",
-      cond = conditions.buffer_not_empty,
-    }
-
-    ins_left {
-      "filename",
-      cond = conditions.buffer_not_empty,
-      color = { fg = colors.magenta, gui = "bold" },
-    }
-
-    ins_left { "location" }
 
     ins_left { "progress", color = { fg = colors.fg, gui = "bold" } }
+
+    ins_left { "location" }
 
     ins_left {
       "diagnostics",
@@ -145,47 +129,8 @@ return {
       function() return "%=" end,
     }
 
-    ins_left {
-      -- Lsp server name .
-      function()
-        local msg = "No Active Lsp"
-        local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-        local clients = vim.lsp.get_active_clients()
-        if next(clients) == nil then return msg end
-        for _, client in ipairs(clients) do
-          local filetypes = client.config.filetypes
-          if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then return client.name end
-        end
-        return msg
-      end,
-      icon = " LSP:",
-      color = { fg = "#ffffff", gui = "bold" },
-    }
-
-    -- Add components to right sections
-    ins_right {
-      "o:encoding", -- option component same as &encoding in viml
-      fmt = string.upper, -- I'm not sure why it's upper case either ;)
-      cond = conditions.hide_in_width,
-      color = { fg = colors.green, gui = "bold" },
-    }
-
-    ins_right {
-      "fileformat",
-      fmt = string.upper,
-      icons_enabled = false, -- I think icons are cool but Eviline doesn't have them. sigh
-      color = { fg = colors.green, gui = "bold" },
-    }
-
-    ins_right {
-      "branch",
-      icon = "",
-      color = { fg = colors.violet, gui = "bold" },
-    }
-
     ins_right {
       "diff",
-      -- Is it me or the symbol for modified us really weird
       symbols = { added = " ", modified = "󰝤 ", removed = " " },
       diff_color = {
         added = { fg = colors.green },
@@ -196,12 +141,11 @@ return {
     }
 
     ins_right {
-      function() return "▊" end,
-      color = { fg = colors.blue },
-      padding = { left = 1 },
+      "branch",
+      icon = "",
+      color = { fg = colors.violet, gui = "bold" },
     }
 
-    -- Now don't forget to initialize lualine
     lualine.setup(config)
   end,
 }
