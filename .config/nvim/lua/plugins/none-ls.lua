1f771310b62e5ea3a09e6725ad64e73fa22f1b38
Ushira Dineth <ushiradineth@gmail.com>
2024-08-16 02:14:08 +0400

    fix: eslint

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -5,39 +5,19 @@ return {
       "nvimtools/none-ls-extras.nvim",
     },
     config = function()
-      local null_ls = require "null-ls"
-      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-      local on_attach = function(client, bufnr)
-        if client.supports_method "textDocument/formatting" then
-          vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function() vim.lsp.buf.format { async = false } end,
-          })
-        end
-      end
-
-      require("lspconfig").eslint.setup {
-        settings = {
-          packageManager = "yarn",
-        },
-        on_attach = function(client, bufnr)
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            command = "EslintFixAll",
-          })
-        end,
-      }
-
-      null_ls.setup {
+      local null_ls = require("null-ls")
+      null_ls.setup({
         sources = {
           null_ls.builtins.formatting.stylua,
+          require "none-ls.diagnostics.eslint_d".with({
+            diagnostics_format = "[eslint] #{m}\n(#{c})",
+          }),
+          require "none-ls.formatting.eslint_d".with({
+            diagnostics_format = "[eslint] #{m}\n(#{c})",
+          }),
           null_ls.builtins.formatting.prettierd,
-          -- require "none-ls.diagnostics.eslint_d",
         },
-        on_attach = on_attach,
-      }
+      })
 
       vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { noremap = true, silent = true, desc = "Format" })
     end,
@@ -50,7 +30,7 @@ return {
       "nvimtools/none-ls.nvim",
     },
     config = function()
-      require("mason-null-ls").setup {
+      require("mason-null-ls").setup({
         ensure_installed = {
           "stylua",
           "easy-coding-standard",
@@ -80,8 +60,9 @@ return {
           "jsonlint",
           "markdownlint",
           "sonarlint-language-server",
+          "yaml-language-server",
         },
-      }
+      })
     end,
   },
 }
