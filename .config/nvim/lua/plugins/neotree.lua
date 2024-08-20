1dc8de7e06e6ca73af5e3d539689d0c5519d1fe9
Ushira Dineth <ushiradineth@gmail.com>
2024-08-20 21:10:48 +0400

    refactor: separate plugins

---
+++ .config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,49 @@
+return {
+  { -- FILE EXPLORER
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+      "3rd/image.nvim",
+    },
+    config = function()
+      require("neo-tree").setup({
+        filesystem = {
+          hijack_netrw_behavior = "open_current",
+          filtered_items = {
+            visible = true,
+            hide_dotfiles = true,
+            hide_gitignored = true,
+          },
+          follow_current_file = {
+            enabled = true,
+            leave_dirs_open = false,
+          },
+        },
+        event_handlers = {
+          {
+            event = "file_opened",
+            handler = function()
+              require("neo-tree").close_all()
+            end,
+          },
+        },
+        window = {
+          mappings = {
+            ["P"] = { "toggle_preview", config = { use_float = false, use_image_nvim = true } },
+          },
+        },
+      })
+
+      vim.keymap.set(
+        "n",
+        "<leader>e",
+        ":Neotree filesystem toggle left<CR>",
+        { noremap = true, silent = true, desc = "Toggle Neotree" }
+      )
+      vim.keymap.set("n", "<C-f>", ":Neotree<CR>", { noremap = true, silent = true })
+    end,
+  },
+}
--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,31 +1,52 @@
 local catppuccin = {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			transparent_background = vim.g.transparent_enabled,
-		})
+  "catppuccin/nvim",
+  name = "catppuccin",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      transparent_background = vim.g.transparent_enabled,
+    })
 
-		vim.cmd.colorscheme("catppuccin-mocha")
-	end,
+    vim.cmd.colorscheme("catppuccin-mocha")
+  end,
 }
 
 local rosepine = {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("rose-pine").setup({
-			styles = {
-				transparent = vim.g.transparent_enabled,
-			},
-		})
+  "rose-pine/neovim",
+  name = "rose-pine",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("rose-pine").setup({
+      styles = {
+        transparent = vim.g.transparent_enabled,
+      },
+    })
 
-		vim.cmd.colorscheme("rose-pine-moon")
-	end,
+    vim.cmd.colorscheme("rose-pine-moon")
+  end,
 }
 
-return rosepine
+return {
+  rosepine, -- ONE OF THE THEMES DEFINED ABOVE
+  { -- UI TRANSPARENCY
+    "xiyaowong/transparent.nvim",
+    config = function()
+      require("transparent").setup({
+        extra_groups = {},
+      })
+
+      require("transparent").clear_prefix("lualine")
+      require("transparent").clear_prefix("NeoTree")
+      require("transparent").clear_prefix("BufferLine")
+
+      vim.keymap.set(
+        "n",
+        "<Leader>t",
+        ":TransparentToggle<CR>",
+        { noremap = true, silent = true, desc = "Toggle Transparency" }
+      )
+    end,
+  },
+}
