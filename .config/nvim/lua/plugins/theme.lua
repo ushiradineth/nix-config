5c11742e6a6a641b6b298ad3dc76734d48097352
Ushira Dineth <ushiradineth@gmail.com>
2024-08-20 16:47:29 +0400

    feat: added custom art

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,25 +1,31 @@
 local catppuccin = {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup()
+	"catppuccin/nvim",
+	name = "catppuccin",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			transparent_background = vim.g.transparent_enabled,
+		})
 
-    vim.cmd.colorscheme "catppuccin-mocha"
-  end,
+		vim.cmd.colorscheme("catppuccin-mocha")
+	end,
 }
 
 local rosepine = {
-  "rose-pine/neovim",
-  name = "rose-pine",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("rose-pine").setup {}
+	"rose-pine/neovim",
+	name = "rose-pine",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("rose-pine").setup({
+			styles = {
+				transparent = vim.g.transparent_enabled,
+			},
+		})
 
-    vim.cmd.colorscheme "rose-pine-moon"
-  end,
+		vim.cmd.colorscheme("rose-pine-moon")
+	end,
 }
 
-return catppuccin
+return rosepine
