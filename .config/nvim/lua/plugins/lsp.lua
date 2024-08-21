31553b8f4b494fe30deee356ddb88d6e80baee94
Ushira Dineth <ushiradineth@gmail.com>
2024-08-21 13:07:47 +0400

    fix: whichkey update

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,83 +1,86 @@
 local function merge(t1, t2)
-  for i = 1, #t2 do
-    t1[#t1 + 1] = t2[i]
-  end
-  return t1
+	for i = 1, #t2 do
+		t1[#t1 + 1] = t2[i]
+	end
+	return t1
 end
 
 local function mappings(bufnr)
-  local opts = { noremap = true, silent = true, buffer = bufnr }
+	local opts = { noremap = true, silent = true, buffer = bufnr }
 
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-  vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, merge({ desc = "Go to Definition" }, opts))
-  vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, merge({ desc = "Code Action" }, opts))
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+	vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, merge({ desc = "Go to Definition" }, opts))
+	vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, merge({ desc = "Code Action" }, opts))
+	vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, merge({ desc = "Rename Definition" }, opts))
 end
 
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function() require("mason").setup {} end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "mason.nvim", "treesitter-terraform-doc.nvim" },
-    config = function()
-      require("mason-lspconfig").setup {
-        ensure_installed = {
-          "typos_lsp",
-          "angularls",
-          "ansiblels",
-          "astro",
-          "bashls",
-          "cmake",
-          "cssls",
-          "diagnosticls",
-          "dockerls",
-          "docker_compose_language_service",
-          "elixirls",
-          "gopls",
-          "grammarly",
-          "html",
-          "helm_ls",
-          "jsonls",
-          "tsserver",
-          "jqls",
-          "lua_ls",
-          "marksman",
-          "nginx_language_server",
-          "spectral",
-          "intelephense",
-          "prismals",
-          "pylsp",
-          "rust_analyzer",
-          "sqlls",
-          "taplo",
-          "tailwindcss",
-          "terraformls",
-          "tflint",
-          "volar",
-          "yamlls",
-        },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "mason.nvim", "treesitter-terraform-doc.nvim" },
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"typos_lsp",
+					"angularls",
+					"ansiblels",
+					"astro",
+					"bashls",
+					"cmake",
+					"cssls",
+					"diagnosticls",
+					"dockerls",
+					"docker_compose_language_service",
+					"elixirls",
+					"gopls",
+					"grammarly",
+					"html",
+					"helm_ls",
+					"jsonls",
+					"tsserver",
+					"jqls",
+					"lua_ls",
+					"marksman",
+					"nginx_language_server",
+					"spectral",
+					"intelephense",
+					"prismals",
+					"pylsp",
+					"rust_analyzer",
+					"sqlls",
+					"taplo",
+					"tailwindcss",
+					"terraformls",
+					"tflint",
+					"volar",
+					"yamlls",
+				},
 
-        require("mason-lspconfig").setup_handlers {
-          function(server_name)
-            require("lspconfig")[server_name].setup {
-              on_attach = function(client, bufnr)
-                mappings(bufnr)
-                require("illuminate").on_attach(client)
-              end,
-            }
-          end,
-        },
-      }
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-  },
-  {
-    -- TERRAFORM DOCS
-    "Afourcat/treesitter-terraform-doc.nvim",
-    dependencies = { "nvim-treesitter" },
-  },
+				require("mason-lspconfig").setup_handlers({
+					function(server_name)
+						require("lspconfig")[server_name].setup({
+							on_attach = function(client, bufnr)
+								mappings(bufnr)
+								require("illuminate").on_attach(client)
+							end,
+						})
+					end,
+				}),
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+	},
+	{
+		-- TERRAFORM DOCS
+		"Afourcat/treesitter-terraform-doc.nvim",
+		dependencies = { "nvim-treesitter" },
+	},
 }
--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,26 +1,35 @@
+local icon = function(icon, color)
+	return {
+		color = color or "blue",
+		icon = icon,
+		cat = "filetype",
+		hl = "WhichKeyIcon",
+	}
+end
+
 return { -- SHORTCUT DISPLAY
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
-  config = function()
-    local wk = require("which-key")
-    opts = {
-      wk.register({
-        c = { name = "Code" },
-        d = { name = "Diagnostics" },
-        g = {
-          name = "Git",
-          r = {
-            name = "Repo",
-          },
-        },
-        l = { name = "Line" },
-        q = { name = "Quit" },
-        s = { name = "Search" },
-      }, { prefix = "<leader>" }),
-    }
-  end,
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"echasnovski/mini.icons",
+		"nvim-tree/nvim-web-devicons",
+	},
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 300
+	end,
+	config = function()
+		local wk = require("which-key")
+    local mini = require("mini.icons")
+		wk.add({
+			{ "<leader>c", group = "Code" },
+			{ "<leader>d", group = "Diagnostics" },
+			{ "<leader>g", group = "Git" },
+			{ "<leader>gr", group = "Repo" },
+			{ "<leader>l", group = "Line", icon = "⋯" },
+			{ "<leader>q", group = "Quit" },
+			{ "<leader>s", group = "Search" },
+      { "<leader>w", group = "Save", icon = mini.get('file', 'TODO')}
+		})
+	end,
 }
