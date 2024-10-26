9e36f8321347d740eb5f654cc5590da200aca385
Ushira Dineth <ushiradineth@gmail.com>
2024-10-26 16:20:16 +0400

    feat: add theme switcher

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,52 +1,85 @@
 local catppuccin = {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup({
-      transparent_background = vim.g.transparent_enabled,
-    })
+	"catppuccin/nvim",
+	name = "catppuccin",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			transparent_background = vim.g.transparent_enabled,
+		})
 
-    vim.cmd.colorscheme("catppuccin-mocha")
-  end,
+		vim.cmd.colorscheme("catppuccin-mocha")
+	end,
 }
 
 local rosepine = {
-  "rose-pine/neovim",
-  name = "rose-pine",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("rose-pine").setup({
-      styles = {
-        transparent = vim.g.transparent_enabled,
-      },
-    })
+	"rose-pine/neovim",
+	name = "rose-pine",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("rose-pine").setup({
+			styles = {
+				transparent = vim.g.transparent_enabled,
+			},
+		})
 
-    vim.cmd.colorscheme("rose-pine-moon")
-  end,
+		vim.cmd.colorscheme("rose-pine-moon")
+	end,
+}
+
+local tokyonight = {
+	"folke/tokyonight.nvim",
+	name = "tokyonight",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("tokyonight").setup({
+			transparent = vim.g.transparent_enabled,
+		})
+
+		vim.cmd.colorscheme("tokyonight-moon")
+	end,
 }
 
 return {
-  rosepine, -- ONE OF THE THEMES DEFINED ABOVE
-  { -- UI TRANSPARENCY
-    "xiyaowong/transparent.nvim",
-    config = function()
-      require("transparent").setup({
-        extra_groups = {},
-      })
+	-- rosepine, -- ONE OF THE THEMES DEFINED ABOVE
+	{ -- UI TRANSPARENCY
+		"xiyaowong/transparent.nvim",
+		config = function()
+			require("transparent").setup({
+				extra_groups = {},
+			})
 
-      require("transparent").clear_prefix("lualine")
-      require("transparent").clear_prefix("NeoTree")
-      require("transparent").clear_prefix("BufferLine")
+			require("transparent").clear_prefix("lualine")
+			-- require("transparent").clear_prefix("NeoTree")
 
-      vim.keymap.set(
-        "n",
-        "<Leader>t",
-        ":TransparentToggle<CR>",
-        { noremap = true, silent = true, desc = "Toggle Transparency" }
-      )
-    end,
-  },
+			vim.keymap.set(
+				"n",
+				"<Leader>tt",
+				":TransparentToggle<CR>",
+				{ noremap = true, silent = true, desc = "Toggle Transparency" }
+			)
+		end,
+	},
+	{
+		"lmantw/themify.nvim",
+		lazy = false,
+		priority = 999,
+		config = function()
+			require("themify").setup({
+				catppuccin,
+				rosepine,
+				tokyonight,
+				"Yazeed1s/minimal.nvim",
+				"default",
+			})
+			vim.keymap.set(
+				"n",
+				"<Leader>ts",
+				":Themify<CR>",
+				{ noremap = true, silent = true, desc = "Theme Switcher" }
+			)
+		end,
+	},
 }
