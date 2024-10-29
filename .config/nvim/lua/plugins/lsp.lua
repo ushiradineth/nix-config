6fec7f643808acddcb19b63689174d61e72774b9
Ushira Dineth <ushiradineth@gmail.com>
2024-10-29 13:04:39 +0400

    feat: yaml schemas

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -39,6 +39,33 @@ local custom_servers = {
 			root_dir = require("lspconfig").util.find_git_ancestor,
 		})
 	end,
+	["yamlls"] = function()
+		require("lspconfig").yamlls.setup({
+			on_attach = function(_, bufnr)
+				mappings(bufnr)
+			end,
+			settings = {
+				yaml = {
+					schemas = {
+						kubernetes = "*.yaml",
+						["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/**/*.{yml,yaml}",
+						["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
+						["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+						["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+						["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+						["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+						["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+						["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+						["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+						["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+						["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+						["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+						["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+					},
+				},
+			},
+		})
+	end,
 }
 
 return {
