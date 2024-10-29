f509e64d99da7a1664f55bd0bf1da8ac38343a2c
Ushira Dineth <ushiradineth@gmail.com>
2024-10-29 13:04:31 +0400

    fix: oil keybind

--- .config/nvim/lua/plugins/filetree.lua
+++ .config/nvim/lua/plugins/filetree.lua
@@ -3,7 +3,7 @@ return {
 		"stevearc/oil.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		keys = {
-			{ "<leader>e", "<cmd>Oil<CR>", desc = "Explorer" },
+			{ "-", "<cmd>Oil<CR>", desc = "Explorer" },
 		},
 		opts = {
 			view_options = {
