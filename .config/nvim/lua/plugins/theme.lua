d329cda203c9ca488aa1f8c7b610481a9e16bef3
Ushira Dineth <ushiradineth@gmail.com>
2024-11-17 21:17:58 +0400

    fix: neotree transparency

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -52,7 +52,7 @@ return {
 			})
 
 			require("transparent").clear_prefix("lualine")
-			-- require("transparent").clear_prefix("NeoTree")
+			require("transparent").clear_prefix("NeoTree")
 
 			vim.keymap.set(
 				"n",
