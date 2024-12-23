a4d75e60cb45cecd5a23203afb0ce2b75bca786b
Ushira Dineth <ushiradineth@gmail.com>
2024-12-23 09:30:26 +0400

    feat: add hardware config

--- hosts/shulab/boot.nix
+++ hosts/shulab/boot.nix
@@ -1,13 +1,14 @@
 {
   pkgs,
   config,
+  lib,
   ...
 }: {
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
-  boot.kernelPackages = pkgs.linuxPackages_6_1;
-  boot.initrd.availableKernelModules = ["xhci_pci" "ehci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "sr_mod" "rtsx_pci_sdmmc"];
-  boot.initrd.kernelModules = ["i915"];
-  boot.kernelModules = ["kvm-intel" "wl"];
-  boot.extraModulePackages = [config.boot.kernelPackages.broadcom_sta];
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_1;
+  boot.initrd.availableKernelModules = lib.mkForce ["xhci_pci" "ehci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "sr_mod" "rtsx_pci_sdmmc"];
+  boot.initrd.kernelModules = lib.mkForce ["i915"];
+  boot.kernelModules = lib.mkForce ["kvm-intel" "wl"];
+  boot.extraModulePackages = lib.mkForce [config.boot.kernelPackages.broadcom_sta];
 }
