0f27a57c3314e4521dbfc39a42e3316f8f8211e9
Ushira Dineth <ushiradineth@gmail.com>
2024-12-24 23:02:50 +0400

    fix: add darwin check

--- home/base/core/zsh.nix
+++ home/base/core/zsh.nix
@@ -1,20 +1,31 @@
-{...}: let
+{pkgs, ...}: let
   path = "$PATH:$HOME/.local/bin:$HOME/go/bin:$HOME/Library/pnpm";
   envExtra = ''
     export PATH="${path}"
     export EDITOR="nvim"
   '';
-  initExtra = ''
-    eval "$(kubectl completion zsh)"
-    eval "$(/opt/homebrew/bin/brew shellenv)"
-    source /opt/homebrew/share/zsh/site-functions/*
+  initExtra =
+    if pkgs.stdenv.isDarwin
+    then ''
+      eval "$(kubectl completion zsh)"
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+      source /opt/homebrew/share/zsh/site-functions/*
 
-    zstyle ':autocomplete:*' default-context history-incremental-search-backward
-    zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-    zstyle ':completion:*' menu no
-    zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
-    zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
-  '';
+      zstyle ':autocomplete:*' default-context history-incremental-search-backward
+      zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+      zstyle ':completion:*' menu no
+      zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+      zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+    ''
+    else ''
+      eval "$(kubectl completion zsh)"
+
+      zstyle ':autocomplete:*' default-context history-incremental-search-backward
+      zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+      zstyle ':completion:*' menu no
+      zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+      zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+    '';
 in {
   programs.zsh = {
     enable = true;
