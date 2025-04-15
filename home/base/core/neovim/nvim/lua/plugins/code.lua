184e25d7f9731207fd5821cc7b7f94970e1bc4ef
Ushira Dineth <ushiradineth@gmail.com>
2025-04-15 23:38:33 +0530

    refactor(nvim): move treesiter to it's own file

--- home/base/core/neovim/nvim/lua/plugins/code.lua
+++ home/base/core/neovim/nvim/lua/plugins/code.lua
@@ -1,37 +1,4 @@
 return {
-	{ -- SYNTAX HIGHLIGHTING
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-					"astro",
-					"bash",
-					"comment",
-					"dockerfile",
-					"go",
-					"hcl",
-					"javascript",
-					"json",
-					"lua",
-					"nix",
-					"rust",
-					"terraform",
-					"tsx",
-					"typescript",
-					"yaml",
-					"helm",
-				},
-				highlight = { enable = true, additional_vim_regex_highlighting = true },
-				indent = { enable = true },
-				incremental_selection = { enable = true },
-				auto_install = true, -- Auto install missing parsers when entering a buffer
-				sync_install = true,
-			})
-
-			vim.treesitter.language.register("markdown", "mdx")
-		end,
-	},
 	{
 		-- QUICK OPEN `%s//gcI`
 		"roobert/search-replace.nvim",
