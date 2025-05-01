74e9a6b6f72cc152e8c31831e459c4fcba8bba79
Ushira Dineth <ushiradineth@gmail.com>
2025-05-01 19:11:54 +0400

    chore: core parity

--- modules/darwin/core.nix
+++ modules/darwin/core.nix
@@ -1,23 +1,20 @@
-# ===================================================================
-# Core Nix configuration
-# ===================================================================
 {lib, ...}: {
+  # Scheduled weekly garbage collection
+  nix.gc = {
+    automatic = lib.mkDefault true;
+    dates = lib.mkDefault "weekly";
+    options = lib.mkDefault "--delete-older-than 7d";
+  };
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
 
-  # Disable auto-optimise-store because of this issue:
-  #   https://github.com/NixOS/nix/issues/7273
-  # "error: cannot link '/nix/store/.tmp-link-xxxxx-xxxxx' to '/nix/store/.links/xxxx': File exists"
   nix.settings.auto-optimise-store = false;
 
-  # Scheduled weekly garbage collection
-  nix.gc = {
-    automatic = lib.mkDefault true;
-    options = lib.mkDefault "--delete-older-than 7d";
-  };
+  nix.channel.enable = false;
 
   system.stateVersion = 5;
 }
--- modules/linux/core.nix
+++ modules/linux/core.nix
@@ -1,11 +1,20 @@
 {lib, ...}: {
+  # Scheduled weekly garbage collection
   nix.gc = {
     automatic = lib.mkDefault true;
     dates = lib.mkDefault "weekly";
     options = lib.mkDefault "--delete-older-than 7d";
   };
 
-  nix.settings.auto-optimise-store = true;
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+
+  nix.settings.auto-optimise-store = false;
 
   nix.channel.enable = false;
+
+  system.stateVersion = 5;
 }
