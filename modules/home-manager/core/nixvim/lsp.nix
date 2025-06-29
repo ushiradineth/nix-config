a19ce38b3f13bf0dbaf6da29bf237bd0ce39b97a
Ushira Dineth <ushiradineth@gmail.com>
2025-06-30 01:36:48 +0400

    feat: add lsps and code plugins

---
+++ modules/home-manager/core/nixvim/lsp.nix
@@ -0,0 +1,72 @@
+{...}: {
+  programs.nixvim.plugins.lsp = {
+    servers = {
+      rust_analyzer.enable = true;
+      eslint = {
+        enable = true;
+        settings = {
+          workingDirectory.mode = "location";
+        };
+      };
+      yamlls = {
+        enable = true;
+        settings = {
+          yaml = {
+            schemaStore = {
+              enable = true;
+              url = "https://www.schemastore.org/api/json/catalog.json";
+            };
+            # schemas = {
+            #   ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/all.json"] = "*.{yml,yaml}";
+            #   ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/**/*.{yml,yaml}";
+            #   ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*";
+            #   ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}";
+            #   ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}";
+            #   ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}";
+            #   ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}";
+            #   ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}";
+            #   ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}";
+            #   ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}";
+            #   ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] =
+            #   "*api*.{yml,yaml}";
+            #   ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] =
+            #   "*docker-compose*.{yml,yaml}";
+            #   ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] =
+            #   "*flow*.{yml,yaml}";
+            #   ["https://api.bitbucket.org/schemas/pipelines-configuration"] = "*bitbucket-pipelines*.{yml,yaml}";
+            #   ["values.schema.json"] = "values.yaml";
+            # };
+            format.enabled = true;
+            validate = true;
+            completion = true;
+            hover = true;
+          };
+        };
+      };
+
+      astro.enable = true;
+      ts_ls.enable = true;
+      volar.enable = true;
+      # prismals.enable = true;
+      tailwindcss.enable = true; # Tailwind LSP
+      jqls.enable = true;
+      jsonls.enable = true;
+      taplo.enable = true; # TOML LSP
+      terraformls.enable = true;
+      ansiblels.enable = true;
+      pylsp.enable = true;
+      dockerls.enable = true;
+      gopls.enable = true; # Go LSP
+      # intelephense.enable = true;
+      lua_ls.enable = true;
+      bashls.enable = true;
+      marksman.enable = true; # Markdown LSP
+      html.enable = true; # HTML LSP
+      cssls.enable = true;
+      diagnosticls.enable = true; # Diagnostics engine
+      helm_ls.enable = true;
+      nginx_language_server.enable = true;
+      typos_lsp.enable = true;
+    };
+  };
+}
