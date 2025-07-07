06281386db551efefd64000045628d82f1dc1c4d
Ushira Dineth <ushiradineth@gmail.com>
2025-07-07 06:58:49 +0400

    fix(nixvim): add performance related tweaks and default required packages

--- modules/home-manager/core/nixvim/default.nix
+++ modules/home-manager/core/nixvim/default.nix
@@ -1,9 +1,29 @@
 {
-  inputs,
+  pkgs,
   mylib,
   ...
 }: {
   imports = mylib.scanPaths ./.;
+
+  home.packages = with pkgs; [
+    lua5_1 # Some packages require lua5_1 to be installed
+    luarocks
+    stylua
+
+    python311 # For plugins that require python
+    nodejs # For plugins that require node
+    go # For plugins that require go
+
+    # Formatters and Linters
+    typos
+    biome
+    prettierd
+    markdownlint-cli
+    beautysh
+    yamlfix
+    alejandra # Nix formatter
+    codespell # Spell checker for code
+  ];
 
   programs.nixvim = {
     enable = true;
@@ -11,32 +31,24 @@
     viAlias = true;
     vimAlias = true;
 
-    performance = {
-      byteCompileLua.enable = true;
-      combinePlugins.enable = true;
+    withNodeJs = false;
+    withPerl = false;
+    withPython3 = false;
+    withRuby = false;
+
+    luaLoader.enable = true;
+
+    performance.byteCompileLua = {
+      enable = true;
+      configs = true;
+      initLua = true;
+      nvimRuntime = true;
+      plugins = true;
     };
-
-    globals.mapleader = " ";
-
-    opts = {
-      expandtab = true;
-      tabstop = 2;
-      softtabstop = 2;
-      shiftwidth = 2;
-      laststatus = 3;
-    };
-
-    clipboard.register = "unnamedplus";
 
     colorschemes.catppuccin = {
       enable = true;
       settings.flavour = "frappe";
-    };
-
-    plugins = {
-      web-devicons.enable = true;
-      telescope.enable = true;
-      treesitter.enable = true;
     };
   };
 }
