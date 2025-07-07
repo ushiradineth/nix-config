220f9b5a01e755a384138fe07f721a386bd023bb
Ushira Dineth <ushiradineth@gmail.com>
2025-07-07 06:29:01 +0400

    feat(nixvim): add git related plugins

---
+++ modules/home-manager/core/nixvim/plugins/git.nix
@@ -0,0 +1,77 @@
+{...}: {
+  programs.nixvim.plugins.gitsigns = {
+    enable = true;
+    settings = {
+      current_line_blame = true;
+      signcolumn = true;
+      watch_gitdir = {
+        follow_files = true;
+      };
+    };
+  };
+
+  programs.nixvim.plugins.diffview.enable = true;
+
+  programs.nixvim.keymaps = [
+    {
+      key = "<leader>gp";
+      mode = ["n"];
+      action = ":Gitsigns preview_hunk<CR>";
+      options = {
+        noremap = true;
+        silent = true;
+        desc = "Preview Hunk";
+      };
+    }
+    {
+      key = "<leader>gd";
+      mode = ["n"];
+      action = ":DiffviewOpen -- %<CR>";
+      options = {
+        noremap = true;
+        silent = true;
+        desc = "Diff";
+      };
+    }
+    {
+      key = "<leader>gdc";
+      mode = ["n"];
+      action = ":DiffviewClose<CR>";
+      options = {
+        noremap = true;
+        silent = true;
+        desc = "Diff Close";
+      };
+    }
+    {
+      key = "<leader>grd";
+      mode = ["n"];
+      action = ":DiffviewOpen<CR>";
+      options = {
+        noremap = true;
+        silent = true;
+        desc = "Diff";
+      };
+    }
+    {
+      key = "<leader>gh";
+      mode = ["n"];
+      action = ":DiffviewFileHistory %<CR>";
+      options = {
+        noremap = true;
+        silent = true;
+        desc = "History";
+      };
+    }
+    {
+      key = "<leader>grh";
+      mode = ["n"];
+      action = ":DiffviewFileHistory<CR>";
+      options = {
+        noremap = true;
+        silent = true;
+        desc = "History";
+      };
+    }
+  ];
+}
