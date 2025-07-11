7cc683c48bed7a385845490550441853a803c8cd
Ushira Dineth <ushiradineth@gmail.com>
2025-07-12 01:11:21 +0400

    fix(transparent): make lualine transparent

--- modules/home-manager/core/nixvim/plugins/theme.nix
+++ modules/home-manager/core/nixvim/plugins/theme.nix
@@ -1,5 +1,42 @@
 {...}: {
-  programs.nixvim.plugins.transparent.enable = true;
+  programs.nixvim.plugins.transparent = {
+    enable = true;
+    luaConfig = {
+      post = ''
+        require("transparent").clear_prefix("lualine")
+      '';
+    };
+    settings = {
+      groups = [
+        "Normal"
+        "NormalNC"
+        "Comment"
+        "Constant"
+        "Special"
+        "Identifier"
+        "Statement"
+        "PreProc"
+        "Type"
+        "Underlined"
+        "Todo"
+        "String"
+        "Function"
+        "Conditional"
+        "Repeat"
+        "Operator"
+        "Structure"
+        "LineNr"
+        "NonText"
+        "SignColumn"
+        "CursorLine"
+        "CursorLineNr"
+        "StatusLine"
+        "StatusLineNC"
+        "EndOfBuffer"
+      ];
+    };
+  };
+
   programs.nixvim.colorschemes.catppuccin = {
     enable = true;
     settings.flavour = "frappe";
