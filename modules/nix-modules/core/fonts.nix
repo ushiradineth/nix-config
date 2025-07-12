90eda970609eb7c5bd0b908d737cd11f6957150c
Ushira Dineth <ushiradineth@gmail.com>
2025-07-12 04:18:02 +0400

    fix: remove font config from darwin

--- modules/nix-modules/core/fonts.nix
+++ modules/nix-modules/core/fonts.nix
@@ -15,16 +15,5 @@
       source-code-pro
       twitter-color-emoji
     ];
-
-    fontconfig = {
-      enable = true;
-      antialias = true;
-      defaultFonts = {
-        serif = ["Gentium"];
-        sansSerif = ["Cantarell"];
-        monospace = ["JetBrainsMono Nerd Font" "MesloLGS Nerd Font Mono" "Source Code Pro"];
-        emoji = ["Twitter Color Emoji"];
-      };
-    };
   };
 }
---
+++ modules/nix-modules/linux/font-config.nix
@@ -0,0 +1,14 @@
+{
+  fonts = {
+    fontconfig = {
+      enable = true;
+      antialias = true;
+      defaultFonts = {
+        serif = ["Gentium"];
+        sansSerif = ["Cantarell"];
+        monospace = ["JetBrainsMono Nerd Font" "MesloLGS Nerd Font Mono" "Source Code Pro"];
+        emoji = ["Twitter Color Emoji"];
+      };
+    };
+  };
+}
