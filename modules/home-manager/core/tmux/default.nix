5f8391a755891d4c5dddcdbb3f0352efa642b36a
Ushira Dineth <ushiradineth@gmail.com>
2025-09-15 01:00:23 +0530

    fix: tmux resurrect and continuum

--- modules/home-manager/core/tmux/default.nix
+++ modules/home-manager/core/tmux/default.nix
@@ -1,4 +1,29 @@
-{config, ...}: {
-  xdg.configFile."../.tmux.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/modules/home-manager/core/tmux/.tmux.conf";
-  programs.tmux.enable = true;
+{pkgs, ...}: {
+  programs.tmux = {
+    enable = true;
+    shell = "${pkgs.zsh}/bin/zsh";
+
+    extraConfig = builtins.readFile ./.tmux.conf;
+
+    plugins = with pkgs.tmuxPlugins; [
+      {
+        plugin = resurrect;
+        extraConfig = ''
+          set -g @resurrect-strategy-vim 'session'
+          set -g @resurrect-strategy-nvim 'session'
+          set -g @resurrect-capture-pane-contents 'on'
+          set -g @resurrect-save 'S'
+          set -g @resurrect-restore 'R'
+        '';
+      }
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-boot 'on'
+          set -g @continuum-save-interval '10'
+        '';
+      }
+    ];
+  };
 }
