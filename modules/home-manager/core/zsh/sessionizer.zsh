455b9c179ef61ef94c3b08d7f787df6f65b28034
Ushira Dineth <ushiradineth@gmail.com>
2025-09-14 23:24:20 +0530

    chore: fmt

--- modules/home-manager/core/zsh/sessionizer.zsh
+++ modules/home-manager/core/zsh/sessionizer.zsh
@@ -1,45 +1,46 @@
 function t() {
-  local selected selected_name tmux_running
+    local selected selected_name tmux_running
 
-  # If an session is passed, use it
-  if [[ $# -eq 1 ]]; then
-    selected=$1
-  else
-    local manual=("$HOME/nix-config" "$HOME/.config")
-    local code=$(find ~/Code -mindepth 1 -maxdepth 1 -type d)
-    local surge=$(find ~/Code/surge -mindepth 1 -maxdepth 2 -type d)
-    local fork=$(find ~/Code/fork -mindepth 1 -maxdepth 1 -type d)
-    local koano=$(find ~/Code/koano -mindepth 1 -maxdepth 1 -type d)
+    # If an session is passed, use it
+    if [[ $# -eq 1 ]]; then
+        selected=$1
+    else
+        local manual=("$HOME/nix-config" "$HOME/.config")
+        local code=$(find ~/Code -mindepth 1 -maxdepth 1 -type d)
+        local surge=$(find ~/Code/surge -mindepth 1 -maxdepth 2 -type d)
+        local fork=$(find ~/Code/fork -mindepth 1 -maxdepth 1 -type d)
+        local koano=$(find ~/Code/koano -mindepth 1 -maxdepth 1 -type d)
+        local freelance=$(find ~/Code/freelance -mindepth 1 -maxdepth 2 -type d)
 
-    # Combine and fzf
-    selected=$(
-      printf '%s\n' "${manual[@]}" "${code[@]}" "${surge[@]}" "${fork[@]}" "${koano[@]}" \
-        | fzf
-    )
-  fi
+        # Combine and fzf
+        selected=$(
+            printf '%s\n' "${manual[@]}" "${code[@]}" "${surge[@]}" "${fork[@]}" "${koano[@]}" "${freelance[@]}" \
+                | fzf
+        )
+    fi
 
-  # Exit if nothing selected
-  [[ -z $selected ]] && return 0
+    # Exit if nothing selected
+    [[ -z $selected ]] && return 0
 
-  # Derive a safe session name
-  selected_name=$(basename -- "$selected" | tr '.' '_')
-  tmux_running=$(pgrep tmux)
+    # Derive a safe session name
+    selected_name=$(basename -- "$selected" | tr '.' '_')
+    tmux_running=$(pgrep tmux)
 
-  # If no tmux server at all, start a fresh session
-  if [[ -z $TMUX && -z $tmux_running ]]; then
-    tmux new-session -s $selected_name -c "$selected"
-    return
-  fi
+    # If no tmux server at all, start a fresh session
+    if [[ -z $TMUX && -z $tmux_running ]]; then
+        tmux new-session -s $selected_name -c "$selected"
+        return
+    fi
 
-  # Create detached session if it doesn't exist
-  if ! tmux has-session -t="$selected_name" 2>/dev/null; then
-    tmux new-session -ds "$selected_name" -c "$selected"
-  fi
+    # Create detached session if it doesn't exist
+    if ! tmux has-session -t="$selected_name" 2>/dev/null; then
+        tmux new-session -ds "$selected_name" -c "$selected"
+    fi
 
-  # Attach or switch
-  if [[ -z $TMUX ]]; then
-    tmux attach-session -t "$selected_name"
-  else
-    tmux switch-client -t "$selected_name"
-  fi
+    # Attach or switch
+    if [[ -z $TMUX ]]; then
+        tmux attach-session -t "$selected_name"
+    else
+        tmux switch-client -t "$selected_name"
+    fi
 }
