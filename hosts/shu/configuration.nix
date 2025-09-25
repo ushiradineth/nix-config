b10bd10019e9e285fcb78a3f1023cbbd72ac59f5
Ushira Dineth <ushiradineth@gmail.com>
2025-09-25 15:11:43 +0530

    fix: sudo with touchid on tmux

--- hosts/shu/configuration.nix
+++ hosts/shu/configuration.nix
@@ -1,6 +1,7 @@
 {self, ...}: {
   # Add ability to used TouchID for sudo authentication
   security.pam.services.sudo_local.touchIdAuth = true;
+  security.pam.services.sudo_local.reattach = true;
 
   system = {
     stateVersion = 5;
