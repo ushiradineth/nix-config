c1fe7175f509fd3df9d75035de20661e0bd15e41
Ushira Dineth <ushiradineth@gmail.com>
2025-10-22 05:22:25 +0530

    feat(shupi): add portainer

---
+++ hosts/shupi/services/portainer.nix
@@ -0,0 +1,28 @@
+{config, ...}: {
+  virtualisation.oci-containers.containers.portainer = {
+    image = "portainer/portainer-ce";
+    autoStart = true;
+    volumes = [
+      "/srv/portainer:/data"
+      "/var/run/docker.sock:/var/run/docker.sock"
+    ];
+    ports = ["127.0.0.1:48002:9000"];
+  };
+
+  services.traefik.dynamicConfigOptions.http = {
+    services.portainer.loadBalancer.servers = [
+      {
+        url = "http://localhost:48002";
+      }
+    ];
+
+    routers.portainer = {
+      rule = "Host(`${config.environment.variables.PORTAINER_DOMAIN}`)";
+      tls = {
+        certResolver = "letsencrypt";
+      };
+      service = "portainer";
+      entrypoints = "websecure";
+    };
+  };
+}
