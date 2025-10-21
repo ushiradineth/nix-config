aae43d72df45fcf9f1f3fcc4ecab2bccfa1fee45
Ushira Dineth <ushiradineth@gmail.com>
2025-10-22 05:22:19 +0530

    feat(shupi): add actualbudget

---
+++ hosts/shupi/services/actualbudget.nix
@@ -0,0 +1,25 @@
+{config, ...}: {
+  virtualisation.oci-containers.containers.actualbudget = {
+    image = "actualbudget/actual-server:latest";
+    autoStart = true;
+    volumes = ["/srv/actualbudget:/data"];
+    ports = ["127.0.0.1:48001:5006"];
+  };
+
+  services.traefik.dynamicConfigOptions.http = {
+    services.actualbudget.loadBalancer.servers = [
+      {
+        url = "http://localhost:48001";
+      }
+    ];
+
+    routers.actualbudget = {
+      rule = "Host(`${config.environment.variables.ACTUALBUDGET_DOMAIN}`)";
+      tls = {
+        certResolver = "letsencrypt";
+      };
+      service = "actualbudget";
+      entrypoints = "websecure";
+    };
+  };
+}
