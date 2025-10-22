9f209d1824c717ec37d051151706c14603ec2e09
Ushira Dineth <ushiradineth@gmail.com>
2025-10-22 07:57:42 +0530

    feat(shupi): add opencloud

---
+++ hosts/shupi/services/opencloud.nix
@@ -0,0 +1,56 @@
+{
+  config,
+  hostname,
+  mysecrets,
+  myvars,
+  ...
+}: {
+  age.secrets.oc-initial-admin-password = {
+    file = "${mysecrets}/${hostname}/oc-initial-admin-password.age";
+    mode = "0500";
+    owner = myvars.username;
+  };
+
+  virtualisation.oci-containers.containers.opencloud = {
+    image = "opencloudeu/opencloud-rolling:latest";
+    autoStart = true;
+    ports = ["127.0.0.1:48004:9200"];
+    entrypoint = "/bin/sh";
+    cmd = [
+      "-c"
+      ''
+        export IDM_ADMIN_PASSWORD=$(cat /run/secrets/initial-admin-password)
+        opencloud server
+      ''
+    ];
+    volumes = [
+      "/srv/opencloud/config:/etc/opencloud"
+      "/srv/opencloud/data:/var/lib/opencloud"
+      "${config.age.secrets.oc-initial-admin-password.path}:/run/secrets/initial-admin-password:ro"
+    ];
+    environment = {
+      OC_URL = "https://${config.environment.variables.OC_DOMAIN}";
+      PROXY_TLS = "false";
+      OC_INSECURE = "false";
+      PROXY_ENABLE_BASIC_AUTH = "false";
+      TZ = myvars.timezone;
+    };
+  };
+
+  services.traefik.dynamicConfigOptions.http = {
+    services.opencloud.loadBalancer.servers = [
+      {
+        url = "http://localhost:48004";
+      }
+    ];
+
+    routers.opencloud = {
+      rule = "Host(`${config.environment.variables.OC_DOMAIN}`)";
+      tls = {
+        certResolver = "letsencrypt";
+      };
+      service = "opencloud";
+      entrypoints = "websecure";
+    };
+  };
+}
