ddd3cddb6bf50ef71a66ca18ade4d34a847dbf69
Ushira Dineth <ushiradineth@gmail.com>
2026-01-20 17:27:52 +0530

    fix(shupi): point docker dns to adguard
    Signed-off-by: Ushira Dineth <ushiradineth@gmail.com>

--- hosts/shupi/networking.nix
+++ hosts/shupi/networking.nix
@@ -6,4 +6,9 @@
   };
 
   networking.nameservers = ["127.0.0.1"]; # Use local AdGuard
+
+  # Ensure containers use AdGuard on the host via docker0 interface
+  virtualisation.docker.daemon.settings = {
+    dns = ["172.17.0.1"];
+  };
 }
