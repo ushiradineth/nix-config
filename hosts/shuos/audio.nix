53a2842e58c775880b530f605d2309f4ffe2b294
Ushira Dineth <ushiradineth@gmail.com>
2026-02-28 19:09:11 +0530

    fix(audio): pipewire bluetooth audio quality
    Signed-off-by: Ushira Dineth <ushiradineth@gmail.com>

--- hosts/shuos/audio.nix
+++ hosts/shuos/audio.nix
@@ -5,6 +5,32 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    wireplumber.enable = true;
+    extraConfig.pipewire = {
+      "10-clock-rate" = {
+        "context.properties" = {
+          "default.clock.allowed-rates" = [44100 48000 96000];
+        };
+      };
+    };
+    wireplumber = {
+      enable = true;
+      extraConfig = {
+        "10-hdmi-audio" = {
+          "monitor.alsa.rules" = [
+            {
+              matches = [{"device.name" = "alsa_card.pci-0000_06_00.1";}];
+              actions = {
+                update-props = {
+                  "api.acp.auto-profile" = true;
+                  "device.profile" = "hdmi-stereo";
+                  "audio.format" = "S32LE";
+                  "audio.rate" = 48000;
+                };
+              };
+            }
+          ];
+        };
+      };
+    };
   };
 }
