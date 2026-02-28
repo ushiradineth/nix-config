3b14ce060fcd3c456a099d428a15ed514c1adc6a
Ushira Dineth <ushiradineth@gmail.com>
2026-02-28 21:25:12 +0530

    feat(flakes): add zig flake
    Signed-off-by: Ushira Dineth <ushiradineth@gmail.com>

---
+++ flakes/zig.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+  in
+    nixpkgs.lib.genAttrs systems (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.zig
+            pkgs.zls
+            pkgs.vimPlugins.nvim-treesitter-parsers.zig
+          ];
+        };
+      }
+    );
+}
