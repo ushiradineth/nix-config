72e14a712e19f58a8b1da22138be7c97a1abc48d
Ushira Dineth <ushiradineth@gmail.com>
2026-07-30 20:39:00 +0530

    chore(shupi): update homepage wallpaper
    Signed-off-by: Ushira Dineth <ushiradineth@gmail.com>

--- hosts/shupi/services/homepage.nix
+++ hosts/shupi/services/homepage.nix
@@ -46,7 +46,7 @@ in {
       favicon = "https://ushira.com/favicon.svg";
       cardBlur = "lg";
       background = {
-        image = "https://sf.shupi.ushira.com/seafhttp/files/ce5e2b0f-5d55-4616-96d9-f96d16795811/2d1baef67bcb2d35e6cbe508c084fe46.png";
+        image = "https://sf.shupi.ushira.com/seafhttp/f/17ce8a9973d542b58e98/?op=view";
         blur = "md";
         saturate = "50";
         brightness = "50";
