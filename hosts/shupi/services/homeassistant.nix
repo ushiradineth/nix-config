d3da94573abfb563f7cbd48ac4740b4cd2c5e4f9
Ushira Dineth <ushiradineth@gmail.com>
2026-08-06 19:24:09 +0530

    feat(shupi): move HA from service to docker
    Signed-off-by: Ushira Dineth <ushiradineth@gmail.com>

--- hosts/shupi/services/homeassistant.nix
+++ hosts/shupi/services/homeassistant.nix
@@ -1,33 +1,56 @@
 {
   config,
   mylib,
+  myvars,
   ...
 }: let
   port = config.ports.homeassistant;
 in {
-  services.home-assistant = {
+  hardware.bluetooth = {
     enable = true;
-    configWritable = true;
-    openFirewall = false;
+    powerOnBoot = true;
+  };
 
-    extraComponents = [
-      "default_config"
-      "dhcp"
-      "esphome"
-      "govee_light_local"
-      "ssdp"
-      "zeroconf"
+  system.activationScripts.homeAssistantConfig = ''
+    install -d -m 0750 /var/lib/hass
+
+    if [ ! -e /var/lib/hass/configuration.yaml ]; then
+      cat > /var/lib/hass/configuration.yaml <<'EOF'
+    default_config:
+
+    http:
+      server_host: 127.0.0.1
+      server_port: ${toString port}
+      use_x_forwarded_for: true
+      trusted_proxies:
+        - 127.0.0.1
+        - ::1
+    EOF
+      chmod 0644 /var/lib/hass/configuration.yaml
+    fi
+  '';
+
+  virtualisation.oci-containers.containers.homeassistant = {
+    image = "ghcr.io/home-assistant/home-assistant:2026.5.4@sha256:ceb1202133a5a036e8b03e20a10eb113186cc2f871968323c6fc6c3fc4205716";
+    autoStart = true;
+    environment.TZ = myvars.timezone;
+    volumes = [
+      "/var/lib/hass:/config"
+      "/run/dbus:/run/dbus:ro"
+      "/run/udev:/run/udev:ro"
     ];
+    extraOptions = [
+      "--network=host"
+      "--privileged"
+    ];
+  };
 
-    config = {
-      default_config = {};
-
-      http = {
-        server_host = "127.0.0.1";
-        server_port = port;
-        use_x_forwarded_for = true;
-        trusted_proxies = ["127.0.0.1" "::1"];
-      };
+  systemd.services.docker-homeassistant = {
+    after = ["dbus.service" "network-online.target"];
+    wants = ["network-online.target"];
+    serviceConfig = {
+      Restart = "on-failure";
+      RestartSec = "10s";
     };
   };
 
